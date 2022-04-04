3afd02b9d459773bd5b814fca0c014ac1a459138
Igor' Golub <greengadzila@gmail.com>
2022-04-04 16:57:26 +0300

    video 54

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -8,18 +8,12 @@ import s from './users.module.css'
 
 export class Users extends React.Component {
 
-   constructor(props) {
-       super(props);
-
-            axios.get(' https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                debugger;
-                this.props.setUsers(response.data.items);
-            });
-
+    componentDidMount() {
+        axios.get(' https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            debugger;
+            this.props.setUsers(response.data.items);
+        });
     }
-
-   
-
 
     render() {
         return (
